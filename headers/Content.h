93ebc49deed06b37709f464b174de7adf5821d95
RubenSystems <ruben.t.j@icloud.com>
2022-07-14 07:19:50 +0100

    added inner content

---
+++ headers/Content.h
@@ -0,0 +1,33 @@
+//
+//  Content.h
+//  RSwCF
+//
+//  Created by Ruben Ticehurst-James on 14/07/2022.
+//
+
+#ifndef Content_h
+#define Content_h
+
+#include "Generatable.h"
+
+
+namespace rswcf {
+	class Content : public Generatable {
+		
+	public:
+		Content(core::Text n_text): text(std::move(n_text)) {}
+		
+		core::Text generate() {
+			return text;
+		}
+		
+	private:
+		core::Text text;
+	};
+	
+	Generatable * content(core::Text text) {
+		return new Content(text);
+	}
+}
+
+#endif /* Content_h */
